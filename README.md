3f72edef47c4d6154391f7eb0b4847fcb5e932da
lijiamao <951572198@qq.com>
2024-05-06 10:48:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# 平台前端
+
+## 运行
+- 安装依赖
+```
+yarn install
+```
+
+- 开发模式运行
+```
+yarn run serve
+```
+
+- 编译项目
+```
+yarn run build
+```
---
+++ src/views/file/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <a-card :bordered="false" size="small">
+    123
+  </a-card>
+</template>
+
+<script>
+export default {
+  name: 'File'
+}
+</script>
+
+<style lang="less" scoped>
+</style>
